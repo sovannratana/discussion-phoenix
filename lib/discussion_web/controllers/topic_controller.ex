9fa860404bb0af4cc99b0a162889951c7e5b8312
sovannratana <sovannratana18@gmail.com>
2022-06-30 10:50:57 +0700

    create edit form to edit and update the title

--- lib/discussion_web/controllers/topic_controller.ex
+++ lib/discussion_web/controllers/topic_controller.ex
@@ -24,8 +24,31 @@ defmodule DiscussionWeb.TopicController do
         |> put_flash(:info, "Topic Created")
         |> redirect(to: Routes.topic_path(conn, :index))
       {:error, changeset} ->
-      IO.inspect(changeset)
+        IO.inspect(changeset)
         render conn, "new.html", changeset: changeset
     end
   end
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    old_topic = Repo.get(Topic, topic_id) # get old topic from database
+    changeset = Topic.changeset(old_topic, topic) # update to new topic from form input
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: Routes.topic_path(conn, :index))
+      {:error, chnageset} ->
+        render conn, "edit.html", changeset: changeset, topic: old_topic
+    end
+  end
+
+  
 end

affb94bec3e664d25274a4d105e0c0ca4c167f44
sovannratana <sovannratana18@gmail.com>
2022-07-01 11:28:15 +0700

    handling auth by create auth_controller

--- lib/discussion_web/controllers/auth_controller.ex
+++ lib/discussion_web/controllers/auth_controller.ex
@@ -1,11 +1,38 @@
 defmodule DiscussionWeb.AuthController do
   use DiscussionWeb, :controller
   plug Ueberauth
+
+  alias Discussion.User
+  alias Discussion.Repo
   
-  def callback(conn, params) do
-    IO.puts("++++++++")
-    IO.inspect(conn)
-    IO.puts("++++++++")
-    IO.inspect(params)
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+    changeset = User.changeset(%User{}, user_params)
+    
+    sign_in(conn, changeset)
+  end
+
+  defp sign_in(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+       conn
+       |> put_flash(:info, "Welcome back")
+       |> put_session(:user_id, user.id)
+       |> redirect(to: Routes.topic_path(conn, :index))
+      
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: Routes.topic_path(conn, :index))
+    end
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user -> 
+        {:ok, user}
+    end
   end
 end
